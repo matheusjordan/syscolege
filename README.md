6136e4920329628551c22d7cc8f09b219e764f7b
matheusjordan <matheusj1599@outlook.com>
2018-10-21 01:16:01 -0300

    Implementação dos Menus de opções

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # syscolege
 Sistema escolar proposto por Gemão na disciplina Programação I
+
+21/10/2018 *VER 1.0* - Implementação do Menu de Opções
--- sysescolar.py
+++ sysescolar.py
@@ -5,16 +5,17 @@ loop = True
 while(loop):
 
 #Menu Inicial
-    esc = int(input("\n-- Menu --\n01- Alunos\n02- Notas\n00- Sair\nEscolha uma opção: "))
-    while(esc > 2 or esc < 0):
+    esc = int(input("\n-- Menu --\n01- Alunos\n02- Notas\n03- Informações sobre alunos\n00- Sair\nEscolha uma opção: "))
+    while(esc < 0 or esc > 3):
         esc = int(input("\nEscolha inválida!\nDigite novamente: "))
     
     else:
-        
-        while(esc == 1):
-            opc = int(input("\n-- Alunos --\n01- Cadastrar Aluno\n02- Remover Aluno\n03- Listagem de Alunos\n04- Buscas\n00- Menu anterior\nEscolha uma opção: "))
 
-            while(opc < 0 or opc > 4):
+        #Menu de Alunos
+        while(esc == 1):
+            opc = int(input("\n-- Alunos --\n01- Cadastrar Aluno\n02- Remover Aluno\n03- Listagem de Alunos\n00- Menu anterior\nEscolha uma opção: "))
+
+            while(opc < 0 or opc > 3):
                 opc = int(input("\nEscolha inválida\nDigite novamente: "))
             
             if(opc == 1):
@@ -25,9 +26,6 @@ while(loop):
 
             elif(opc == 3):
                 print("opc 3")
-
-            elif(opc == 4):
-                print("opc 4")
             
             else:
                 esc = 10
@@ -50,6 +48,31 @@ while(loop):
 
             elif(opc == 4):
                 print("opc 4")
+
+            else:
+                esc = 10
+
+        #Menu de Informações
+        while(esc == 3):
+            opc = int(input("\n-- Menu de Informações --\n01- Consultar Aluno por nome\n02- Exbir melhor Aluno\n03- Alunos aprovados \n04- Alunos na final\n05- Alunos reprovados\nEscolha uma opção: "))
+
+            while(esc < 0 or esc > 5):
+                opc = int(input("\nEscolha inválida\nDigite novamente: "))
+
+            if(opc == 1):
+                print("opc 1")
+        
+            elif(opc == 2):
+                print("opc 2")
+
+            elif(opc == 3):
+                print("opc 3")
+
+            elif(opc == 4):
+                print("opc 4")
+        
+            elif(opc == 5):
+                print("opc 5")
 
             else:
                 esc = 10
