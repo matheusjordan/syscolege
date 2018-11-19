798ec3e139bad45b844ad7c99bfe51d083683a27
Matheus Jordan <matheusj1599@outlook.com>
2018-11-19 15:28:34 -0200

    correção de ordenação das notas

--- Alunos.py
+++ Alunos.py
@@ -64,7 +64,7 @@ def listAlunos(alunosDic):
             media = sum(alunosDic[aluno]) / 3
             
             while(media in alunosM):
-                media += 1
+                media += 0.1
             
             alunosM[media] = aluno
         
